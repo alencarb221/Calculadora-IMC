327b19d535c47302cb4eecd498cb4324238aafe2
Alencar Baptistel <alencarbatistel47@gmail.com>
2025-05-28 20:42:07 -0300

    Atualização calculadora IMC

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function calcular() {
                 res.innerHTML = 'Preencha todos os campos!'
              } else if(imc < 18.5) {
                 res.innerHTML = `Seu IMC é ${imc} e você está <strong>ABAIXO</strong> do peso`
-            } else if(imc > 18.5 && imc < 25) {
+            } else if(imc < 25) {
                res.innerHTML = `Seu IMC é ${imc} e você está no seu peso <strong>NORMAL</strong>`
-            } else if(imc > 25 && imc < 30) {
+            } else if(imc < 30) {
                 res.innerHTML = `Seu IMC é ${imc} e você está com <strong>SOBREPESO</strong>`
-            } else if(imc > 30 && imc < 35 ) {
+            } else if(imc < 35 ) {
                 res.innerHTML = `Seu IMC é ${imc} e você está com <strong>OBESIDADE GRAU I</strong>`
-            } else if(imc > 35 && imc < 40 ) {
+            } else if(imc < 40 ) {
                 res.innerHTML = `Seu IMC é ${imc} e você está com <strong>OBESIDADE GRAU II(SEVERA)</strong>`
             } else if(imc > 40) {
                 res.innerHTML = `Seu IMC é ${imc} e você está com <strong>OBESIDADE GRAU III(MÓRBIDA)</strong>`
