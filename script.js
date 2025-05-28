3096f661efb2d736596b356adcd107521a9ffe7a
Alencar Baptistel <alencarbatistel47@gmail.com>
2025-05-28 20:35:18 -0300

    Subindo mini projeto calculadora IMC

---
+++ script.js
@@ -0,0 +1,22 @@
+function calcular() {
+            let peso = document.getElementById('peso').value
+            let altura = document.getElementById('altura').value
+            let res = document.getElementById('resultado')
+           
+            let imc = parseFloat((peso / (altura * altura)).toFixed(2))
+             if(peso === '' || altura === '') {
+                res.innerHTML = 'Preencha todos os campos!'
+             } else if(imc < 18.5) {
+                res.innerHTML = `Seu IMC é ${imc} e você está <strong>ABAIXO</strong> do peso`
+            } else if(imc > 18.5 && imc < 25) {
+               res.innerHTML = `Seu IMC é ${imc} e você está no seu peso <strong>NORMAL</strong>`
+            } else if(imc > 25 && imc < 30) {
+                res.innerHTML = `Seu IMC é ${imc} e você está com <strong>SOBREPESO</strong>`
+            } else if(imc > 30 && imc < 35 ) {
+                res.innerHTML = `Seu IMC é ${imc} e você está com <strong>OBESIDADE GRAU I</strong>`
+            } else if(imc > 35 && imc < 40 ) {
+                res.innerHTML = `Seu IMC é ${imc} e você está com <strong>OBESIDADE GRAU II(SEVERA)</strong>`
+            } else if(imc > 40) {
+                res.innerHTML = `Seu IMC é ${imc} e você está com <strong>OBESIDADE GRAU III(MÓRBIDA)</strong>`
+            }
+            }
